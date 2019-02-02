157f442ef37b2ffa9bf4e0f7911a12dacb923ec1
Vasudevan <vasu3797@gmail.com>
2019-02-02 16:34:36 +0530

    calibration

--- testing.py
+++ testing.py
@@ -1,16 +1,13 @@
-from math import pow
+from math import pow, sqrt
 from fetch_rssi import get_rssi
 def distance_calc():
-    tx = -22
+    tx = -35
     rssi = get_rssi()
-    ratio = rssi*1.0/tx
-    if ratio < 1.0:
-        dis = pow(ratio, 10)
-        print(dis)
-    else:
-        dis = 0.89976*pow(ratio, 7.7095) + 0.111
-        print(dis)
+    ratio = tx - rssi
+    ratio_lin = pow(10, ratio/10)
 
+    d = sqrt(ratio_lin)
+    print(d)
 
 if __name__ == '__main__':
     distance_calc()
