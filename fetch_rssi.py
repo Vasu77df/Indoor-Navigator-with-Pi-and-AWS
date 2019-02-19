6f4a6014a306662b41d13d9d03f50e8a4b39e84b
vasu3797@gmail.com <vasu3797@gmail.com>
2019-02-19 16:35:33 +0530

    using mac address

--- fetch_rssi.py
+++ fetch_rssi.py
@@ -1,29 +1,28 @@
 import subprocess as sb
 from time import sleep
 
-def get_rssi():
+def get_apinfo():
     cmnd = " sudo iw wlp8s0 scan|egrep 'SSID|signal'"
-    net = sb.Popen(cmnd, shell=True, stdout=sb.PIPE, stderr=sb.STDOUT)
-
-    net_out = net.communicate()[0]
+    net = sb.run(cmnd, shell=True, stdout=sb.PIPE, stderr=sb.STDOUT)
+    net_out = net.stdout
     net_out = net_out.decode('utf-8')
     net_out = net_out.strip('\n')
     net_out = net_out.split()
-    ssid_pos = net_out.index('OnePlus')
-    rssi_value = net_out[ssid_pos - 3]
-    print(rssi_value)
-    rssi_value = float(rssi_value)
+    return net_out
+
+
+def rssi_parser(net_out):
+    reg_user = '94:65:2D:CB:55:8C'
+    if reg_user in net_out:
+        ssid_pos = net_out.index('94:65:2D:CB:55:8C')
+        rssi_value = net_out[ssid_pos + 3]
+    else:
+        rssi_value = 0
+        sleep(5)
     return rssi_value
 
 if __name__ == "__main__":
     while True:
-        try:
-            get_rssi()
-        except ValueError:
-
-            print("registered ssid not found here")
-            sleep(5)
-            pass
-
-
-
+        net_info = get_apinfo()
+        rssi = rssi_parser(net_info)
+        print(rssi)
