7df5c25d035e0fd73d62a94d890e38451d3875c4
vasu3797@gmail.com <vasu3797@gmail.com>
2019-03-14 17:51:28 +0530

    able to get data from all three tables

--- interfaceskill.js
+++ interfaceskill.js
@@ -1,5 +1,5 @@
 const Alexa = require('ask-sdk');
-const aws =  require('aws-sdk');
+
 
 
 const SKILL_NAME ;
@@ -9,9 +9,73 @@ const HELP_REPROMPT;
 const STOP_MESSAGE = '<say-as interpret-as="interjection">balle balle</say-as>' ;
 
 function currentLocation() {
-    var dbb = new aws.DynamoDB({apiVersion: '2012-10-08'});
+  var AWS = require("aws-sdk");
 
-}
+  AWS.config.update({
+   region: "eu-west-1",
+   endpoint: "https://dynamodb.eu-west-1.amazonaws.com"
+  });
+
+  var docClient = new AWS.DynamoDB.DocumentClient();
+
+
+  var params_one = {
+    TableName: "threeBdataTable",
+    ProjectionExpression: "#ix, rssi_value_threeB, now_time",
+    FilterExpression: "now_time between :first and :last",
+    ExpressionAttributeNames: {
+   "#ix": "index",
+   },
+    ExpressionAttributeValues: {
+      ":first":1552565040980,
+      ":last":1552565087912
+    }
+  };
+
+  var params_two = {
+    TableName: "threeDataTable",
+    ProjectionExpression: "#ix, rssi_value_three, now_time",
+    FilterExpression: "now_time between :first and :last",
+    ExpressionAttributeNames: {
+   "#ix": "index",
+   },
+    ExpressionAttributeValues: {
+      ":first":1552565037028,
+      ":last":1552565074419
+    }
+  };
+
+  var params_three = {
+    TableName: "zeroDataTable",
+    ProjectionExpression: "#ix, rssi_value_zero, now_time",
+    FilterExpression: "now_time between :first and :last",
+    ExpressionAttributeNames: {
+   "#ix": "index",
+   },
+    ExpressionAttributeValues: {
+      ":first":1552565046449,
+      ":last":1552565080265
+    }
+  };
+
+  console.log("Scanning the databaseone");
+  docClient.scan(params_one, onScan);
+  console.log("Scanning the databasetwo");
+  docClient.scan(params_two, onScan);
+  console.log("Scanning the databasethree");
+  docClient.scan(params_three, onScan);
+
+  function onScan(err,data) {
+    if (err) {
+      console.error("Unable to scan the database. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+      console.log("Scan successful.");
+      console.log(data.Items);
+    }
+
+};
+
+};
 
 
 const CurrentLocationHandler = {
@@ -112,6 +176,8 @@ handle(handlerInput) {
 }
 };
 
+
+const skillBuilder = Alexa.SkillBuilders.custom();
 exports.handler = skillBuilder
   .addRequestHandlers(
     CurrentLocationHandler,
